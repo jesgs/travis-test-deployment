fede1bb78dd252a96068f8ab8225b8785fc9b134
Jess Green <ardath.ksheyna78@gmail.com>
2017-11-17 21:36:45 -0600

    Changing directory

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git config --global push.default matching
-git remote add production ssh://test-deploy@psy-dreamer.com:9922/home/test-deploy/travis-test-deployment
+git remote add production ssh://test-deploy@psy-dreamer.com:9922/home/test-deploy/live-site
 git push production master
