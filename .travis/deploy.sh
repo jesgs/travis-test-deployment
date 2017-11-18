f6cf7b4df4da70d5f54708751a82be7e0d0f5f93
Jess Green <ardath.ksheyna78@gmail.com>
2017-11-17 22:20:05 -0600

    Changing to push.default simple

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git config --global push.default matching
+git config --global push.default simple
 git remote add production ssh://test-deploy@psy-dreamer.com:9922/home/test-deploy/live-site
 git push production master
